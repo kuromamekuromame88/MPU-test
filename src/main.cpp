58dc49d573bb7adb823516c189ea58fc5bddb03f
kuromamekuromame88 <tei8sia@outlook.jp>
2025-11-10 22:59:46 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,31 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <MPU9250_asukiaaa.h>
 
-#define SDA_PIN 5
-#define SCL_PIN 4
+#define SDA_PIN 7
+#define SCL_PIN 6
+
+MPU9250_asukiaaa mySensor;
 
 void setup() {
   Serial.begin(115200);
   Wire.begin(SDA_PIN, SCL_PIN);
-  Serial.println("I2C Scanner start...");
 
-  for (uint8_t addr = 1; addr < 127; addr++) {
-    Wire.beginTransmission(addr);
-    if (Wire.endTransmission() == 0) {
-      Serial.printf("Found device at 0x%02X\n", addr);
-      delay(10);
-    }
-  }
+  mySensor.setWire(&Wire);
+  mySensor.beginAccel();
+  mySensor.beginGyro();
+  mySensor.beginMag();
 
-  Serial.println("Scan complete.");
+  Serial.println("MPU9250 check start");
 }
 
-void loop() {}
+void loop() {
+  mySensor.accelUpdate();
+  mySensor.gyroUpdate();
+
+  Serial.printf("Accel: X:%7.2f Y:%7.2f Z:%7.2f  ",
+                mySensor.accelX(), mySensor.accelY(), mySensor.accelZ());
+  Serial.printf("Gyro: X:%7.2f Y:%7.2f Z:%7.2f\n",
+                mySensor.gyroX(), mySensor.gyroY(), mySensor.gyroZ());
+  delay(500);
+}
